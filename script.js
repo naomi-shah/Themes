665ce95af73bc906d4a1dc09f26a81063d214380
naomi-shah <37555446+naomi-shah@users.noreply.github.com>
2019-03-07 09:30:44 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".blue-theme").click(function() {
+  $("img").css("border-color", "blue");
+  $("h1").css("background-color", "blue");
+  $("p").css("color", "blue");
+});
+
+$(".green-theme").click(function() {
+  $("img").css("border-color", "green");
+  $("h1").css("background-color", "green");
+  $("p").css("color", "green");
+});
